6088160f9799a3a068a8a1c395f1085ce578c276
Ted <obrienteddy@hotmail.com>
2022-12-10 21:39:44 -0600

    feat: You are now able to favorite parks on the detail pages as long as you are a logged in user

--- accounts_api/routers/favorites.py
+++ accounts_api/routers/favorites.py
@@ -11,7 +11,7 @@ from models import (
 from .auth import authenticator
 from models import AccountOut, Account
 from routers.sockets import socket_manager
-from queries.favorites import ParkBooleanQueries
+from queries.favorites import FavoriteQueries
 
 router = APIRouter()
 
@@ -20,9 +20,36 @@ async def create_favorite(
     favorite: FavoriteIn,
     response: Response,
     account_data: dict = Depends(authenticator.get_current_account_data),
-    repo: ParkBooleanQueries = Depends(),
+    repo: FavoriteQueries = Depends(),
 ):
     print(account_data)
     new_favorite = repo.create_favorite(account_data["id"], favorite)
     await socket_manager.broadcast_refetch()
     return new_favorite
+
+
+@router.get("/api/favorites/{account_id}", response_model=List[FavoriteOut])
+async def get_favorites(
+    account_id: str,
+    response: Response,
+    account_data: dict = Depends(authenticator.get_current_account_data),
+    repo: FavoriteQueries = Depends(),
+):
+    if account_id == account_data["id"]:
+        favorites = repo.get_all_favorites_in_account(account_id)
+        await socket_manager.broadcast_refetch()
+        return favorites
+    else:
+        return "Account ID does not match current user"
+
+
+@router.delete("/api/favorites/{account_id}/{park_code}", response_model=bool)
+async def delete_favorites(
+    account_id: str,
+    park_code: str,
+    account_data: dict = Depends(authenticator.get_current_account_data),
+    repo: FavoriteQueries = Depends(),
+):
+    repo.delete_favorites(account_id=account_id, park_code=park_code)
+    await socket_manager.broadcast_refetch()
+    return True
